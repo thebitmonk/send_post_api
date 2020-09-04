07a2d2039b7f96839423615ae2174dbf62a2d474
John <john@Johns-MacBook-Air.local>
2020-09-04 22:03:35 +0530

    added new swagger yaml

--- src/ApiClient.js
+++ src/ApiClient.js
@@ -49,7 +49,7 @@
      * @type {String}
      * @default https://localhost/api/v1
      */
-    this.basePath = 'https://localhost:8081/api/v1'.replace(/\/+$/, '');
+    this.basePath = 'https://localhost/api/v1'.replace(/\/+$/, '');
 
     /**
      * The authentication methods to be included for all API calls.
